caedd42315f782fc40f1c2b868053ad22582560c
Diogo Key <diogo.key@mowa.com.br>
2013-09-13 13:59:55 -0300

    Alterando os conteudos para refletir melhor o estudo cronologico dos exemplos

--- ex01.rb
+++ ex01.rb
@@ -1,44 +1,13 @@
-# Exemplo do uso do Sinatrarb.
-# esse exemplo cria uma aplicação que deve se comportar como um encurtador de URLs.
-# A arquitetura é simples, basta que para uma dada URL um hash de até 8 caracteres 
-# é gerado que remete ao link depois.
-
-
 require 'sinatra'
-require 'active_record'
-require 'mysql2'
 
-ActiveRecord::Base.establish_connection(
-	:adapter => "mysql2",
-	:host => "localhost",
-	:username => "root",
-	:password => "",
-	:database => "workshop_sinatra"
-	)
-
-class Item < ActiveRecord::Base
+get '/hi' do
+	"hello world"
 end
 
-if (!ActiveRecord::Base.connection.table_exists? 'items')
-	ActiveRecord::Migration.create_table :items do |t|
-		t.string :url
-		t.integer :count, :default => 0
-		t.string :name
-	end
+get '/sum/:x' do
+	"soma: #{params[:x]} + 1: " + (params[:x].to_i+1).to_s
 end
 
-class App < Sinatra::Application
-end
-
-get '/:name' do
-	a = Item.find_by_name(params[:name])
-	a.count += 1
-	a.save
-	redirect a.url
-end
-
-post '/create' do
-	a = Item.new
-	a.url, a.name = params[:url], params[:name]
-	a.save
+use Rack::Auth::Basic do |username, password|
+  username == 'admin' && password == 'secret'
 end
--- shortner.rb
+++ shortner.rb
@@ -1,25 +1,44 @@
+# Exemplo do uso do Sinatrarb.
+# esse exemplo cria uma aplicação que deve se comportar como um encurtador de URLs.
+# A arquitetura é simples, basta que para uma dada URL um hash de até 8 caracteres 
+# é gerado que remete ao link depois.
+
+
 require 'sinatra'
 require 'active_record'
-require 'sqlite3'
+require 'mysql2'
 
 ActiveRecord::Base.establish_connection(
-	:adapter => "sqlite3",
-	:database => dev.sqlite3,
-	:pool => 5,
-	:timeout => 5000
+	:adapter => "mysql2",
+	:host => "localhost",
+	:username => "root",
+	:password => "",
+	:database => "workshop_sinatra"
 	)
 
 class Item < ActiveRecord::Base
 end
 
-ActiveRecord::Migration.create_table :items do |t|
-	t.string :name,
-	t.string :original_url
+if (!ActiveRecord::Base.connection.table_exists? 'items')
+	ActiveRecord::Migration.create_table :items do |t|
+		t.string :url
+		t.integer :count, :default => 0
+		t.string :name
+	end
 end
 
 class App < Sinatra::Application
 end
 
-get '/' do
-	p Item.all
+get '/:name' do
+	a = Item.find_by_name(params[:name])
+	a.count += 1
+	a.save
+	redirect a.url
+end
+
+post '/create' do
+	a = Item.new
+	a.url, a.name = params[:url], params[:name]
+	a.save
 end
